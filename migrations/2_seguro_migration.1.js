643c31d7dddcb29608b0e9efe4fafe8422d3e31d
Alex Silva <alexmsilva@gmail.com>
2018-06-07 19:05:05 -0300

    removed unused files

--- migrations/2_seguro_migration.1.js
+++ migrations/2_seguro_migration.1.js
@@ -2,7 +2,8 @@ var Seguro = artifacts.require("../contracts/Seguro.sol");
 
 module.exports = function(deployer, network, accounts) {
 	//address benef, uint256 numeroApolice, uint256 valorDoBemSegurado, uint256 valorDoBem, uint256 valorDaFranquia  
-  	deployer.deploy(Seguro,
+  	deployer.deploy(
+        Seguro,
   		accounts[1],
   		1234,
         1000.00,
