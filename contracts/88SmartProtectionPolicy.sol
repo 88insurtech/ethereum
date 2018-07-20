4ff46f563f94e292b426ea7c89835c179e145b57
Alex Braz <alexjavabraz@gmail.com>
2018-07-20 13:45:25 -0300

    Adding claim, comission, validations

--- contracts/88SmartProtectionPolicy.sol
+++ contracts/88SmartProtectionPolicy.sol
@@ -1,34 +1,41 @@
 pragma solidity ^0.4.24;
 
-contract 88SmartProtectionPolicy {
+contract SmartProtectionPolicy {
 
 
-    enum StatusPolicy { VIGENTE, LIQUIDADO, INATIVO, SINISTRO }
+    enum StatusPolicy { ACTIVE, FINISHED, INACTIVE, CONTAIN_CLAIM }
 
     StatusPolicy status;
-    StatusPolicy constant defaultStatus = StatusApolice.VIGENTE;
+    StatusPolicy constant defaultStatus = StatusPolicy.ACTIVE;
     Policy policy;
-    address owner;
+    address owner; // 88 Ethereum Account
     uint8 public decimals = 18;
 
     struct Policy {
         /** Contains the policy start date and time **/
-        uint16 startPolicyTime;        
+        uint256 startPolicyTime;        
         uint256 policyNumber;
         uint256 valueOfProperty;
-        uint16 premium;
+        uint256 franchiseValue;
+        string insuredItem;
+        string customerName;
+        
+        /** The address of the customer created by mobile aplication **/
+        address customer;
+        
+        uint256 premium;
         uint256 deductablePaymentValue;
-        uint16 duration = 1 years;
+        uint256 contractDuration;
+        
+        uint256 policyBalanceValue;
     }
 
-    /** The address of the customer created by mobile aplication **/
-    address customer;
 
     /** The address of the agent, the dealer **/
     address agent;
 
     /** The address of the broker **/
-    address broker;    
+    address broker;  
 
     /** This event changes the Apolice Status, and is called when something very important occurs **/
     event ChangeStatus(StatusPolicy status, uint256 eventValue);
@@ -47,17 +54,79 @@ contract 88SmartProtectionPolicy {
 
 
     /** The percent of fee **/
-    uint16 comissionFeePercent;
+    uint16 comissionFeePercent = 2;
 
-    /** The percent of fee to the Broker **/
-    uint16 comissionFeeBrokerPercent;
+    /** The percent (%) of fee to the Broker **/
+    uint16 comissionFeeBrokerPercent = 1;
 
-    /** The percent of fee to the agent **/
-    uint16 comissionFeeAgentPercent;
+    /** The percent (%) of fee to the agent **/
+    uint16 comissionFeeAgentPercent = 1;
 
-    /**  **/
-    uint16 comissionFeeBrokerValue;
-    uint16 comissionFeeAgentValue;
+    /** The comission values for stakeholders **/
+    uint16 comissionFeeBrokerValue;/** The value of fee in Ether to be sended to Broker **/
+    uint16 comissionFeeAgentValue;/** The value of fee in Ether to be sended to Agent **/
+    
+    function notZero(uint16 _value) private {
+        require (_value != 0x0);
+    }
+
+    modifier onlyOwner() {
+        if (msg.sender != owner) {
+            throw;
+        }
+        _;
+    }
+    
+    constructor (address acustomeraddress, string acustomerName, uint256 apolicynumber, uint256 avalueOfProperty, uint256 apremium, uint256 afranchise, string ainsuredItem) public {
+        owner = msg.sender;
+
+        policy = Policy({
+             startPolicyTime : now,
+            policyNumber : apolicynumber,
+            valueOfProperty : avalueOfProperty,
+            franchiseValue : afranchise,
+            insuredItem : ainsuredItem,
+            customerName : acustomerName,
+            customer : acustomeraddress,
+            premium : apremium,
+            deductablePaymentValue : apremium,
+            contractDuration : 1 years,
+            policyBalanceValue : apremium
+        });
+
+        emit ChangeStatus(defaultStatus, apremium);
+    }
+    
+
+    /**
+     * 
+    **/
+    function setFeeComissionsPercent(uint16 broker, uint16 agent) onlyOwner() public returns (bool){
+        /**Avoid zero fee comission **/
+        notZero(broker);
+        notZero(agent);
+        
+        /** The percent (%) of fee to the Broker **/
+        comissionFeeBrokerPercent = broker;
+
+        /** The percent (%) of fee to the agent **/
+        comissionFeeAgentPercent = agent; 
+        
+        /** The percent of fee **/
+        comissionFeePercent = broker + agent;
+        
+        return true;
+    }
+    
+    /**
+     * Creates a new claim
+     **/
+    function createClaim() onlyOwner public returns (uint256){
+        return 0;
+    }
+
+
+
 
 
 }
